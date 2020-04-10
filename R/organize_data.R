2ab58a0e4ddea9aa24e324fd8f396651e170bbe3
Tamas Kiss <tamas-kiss@ouhsc.edu>
2020-04-10 23:33:15 +0200

    getDE functio is updated: check the arguments first; argument names changed; used function specified with their source package; return: de gene vector

--- R/organize_data.R
+++ R/organize_data.R
@@ -2,46 +2,84 @@
 #'
 #' \code{getDE} read the DESeq output file and creat or rewrite the DE column based on the manualy set fold change and p-value thesholds.
 #'
-#' @param fileN file name in the data folder
-#' @param fct fold change threshold
-#' @param pvt p-value threshold
+#' @param file_name file name in the data folder
+#' @param fold_change fold change threshold
+#' @param p_value p-value threshold
+#' @param path path to the DESeq files
 #'
-#' @return
+#' @return vector of differentially expressed genes
 #'
 #' @examples
 #'
 
-getDE <- function(fileN, fct, pvt){
+getDE <- function(file_name, fold_change, p_value, path = './data/raw/'){
+
+  # check input quality
+  if(!is.character(file_name)){
+    stop('File name has to be character verctor!')
+  }
+
+  if(!is.numeric(fold_change)){
+    stop('Fold change has to be numeric!')
+  }
+
+  if(fold_change <= 0){
+    stop('Fold change has to be positive number!')
+  }
+
+  if(!is.numeric(p_value)){
+    stop('p-value has to be numeric!')
+  }
+
+  if(p_value > 1| p_value < 0){
+    stop('p-value has to be a number between 0 and 1')
+  }
+
   # read data file
-  df <- read_excel(
-    paste('./data/raw/', fileN, '.xlsx', sep = '')
+  df <- readxl::read_excel(
+    paste(path, file_name, '.xlsx', sep = '')
   )
+
+  # check if the data frame has the right columns
+  if(
+    sum(c('log2FoldChange', 'padj')%in% colnames(df)) != 2
+  ){
+    stop('Right columns are missing! \nThe function needs: \'log2FoldChange\' and \'padj\'')
+  }
 
   # create new variables: DE, FCsize, GRank
   df <- df%>%
-    mutate(DE = if_else(
-      (log2FoldChange < -log(fct,2) | log2FoldChange > log(fct,2)) & padj < pvt,
+    dplyr::mutate(DE = if_else(
+      (log2FoldChange < -log(fold_change,2) | log2FoldChange > log(fold_change,2)) & padj < p_value,
       'DE',
       'notDE',
       missing = 'notDE'
     ))%>%
-    mutate(FCsize = if_else(
-      log2FoldChange < -log(fct,2) | log2FoldChange > log(fct,2),
+    dplyr::mutate(FCsize = if_else(
+      log2FoldChange < -log(fold_change,2) | log2FoldChange > log(fold_change,2),
       'FCbig',
       'FCsmall'
     ))%>%
-    mutate(GRank = dense_rank(-log2FoldChange))
+    dplyr::mutate(GRank = dense_rank(-log2FoldChange))
 
   # drop lines without gene name (these are 'retired' ensembl genes)
   df <- df%>%
-    filter(!is.na(Gene))
+    dplyr::filter(
+      !is.na(Gene)
+      )
 
   # write result
-  write.xlsx(
+  openxlsx::write.xlsx(
     df,
-    paste('./data/raw/', fileN, '.xlsx', sep = '')
+    paste(path, file_name, '.xlsx', sep = '')
   )
 
   # quality check
-  return(dim(df)[1])
+  genes <- df%>%
+    dplyr::filet(DE == 'DE')%>%
+    pull('Ensemb_ID')%>%
+    unique()
+
+  # return gene vector
+  return(genes)
 }
